9e0fe7fe0843202b21cfc8d28008712136a9f2ed
Iskander Romdhane <skander20034@gmail.com>
2024-09-22 13:26:52 +0100

    Email Verification

---
+++ Backend/src/main/java/com/Foody/Foody/email/EmailTemplateName.java
@@ -0,0 +1,13 @@
+package com.Foody.Foody.email;
+
+public enum EmailTemplateName {
+
+    ACTIVATE_ACCOUNT("activate_account")
+    ;
+
+
+    private final String name;
+    EmailTemplateName(String name) {
+        this.name = name;
+    }
+}
