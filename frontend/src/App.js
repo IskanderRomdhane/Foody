0345065e7d0602d4aba6a22a14fcc58f16ad514e
Iskander Romdhane <skander20034@gmail.com>
2024-10-02 00:10:44 +0100

    Routing for new Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route , Routes } from 'react-router-dom';
 import Root from './pages/Root';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Register from './pages/register/Register';
+import Welcome from './pages/Welcome';
 function App() {
   return (
 
       <Router>
         <Routes>
           <Route path='/' Component={Root} />
+          <Route path='/login' Component={Login} />
+          <Route path='/user/home' Component={Home} />
+          <Route path='/user/register' Component={Register} />
+          <Route path= '/user/welcome' Component={Welcome} />
         </Routes>
       </Router>
 
